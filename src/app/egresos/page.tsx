ae3e6fdedd61284242852a135ae0dda1baf4b45e
kromatic97 <rogiromero@gmail.com>
2025-08-01 11:41:56 -0300

    agregar formulario agresos

--- src/app/egresos/page.tsx
+++ src/app/egresos/page.tsx
@@ -1,8 +1,10 @@
+// src/app/egresos/page.tsx
 import EgresoForm from './EgresoForm';
 
-export default function PageEgresos() {
+export default function Page() {
   return (
     <div className="p-4">
+      <h1 className="text-xl font-bold mb-4">Registrar nuevo egreso</h1>
       <EgresoForm />
     </div>
   );
