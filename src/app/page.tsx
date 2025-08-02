e68e0d1b55b1fc55c67204e59869b0677bc83a68
kromatic97 <rogiromero@gmail.com>
2025-08-02 09:03:15 -0300

    configurando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 
 
 import EgresoForm from './egresos/EgresoForm';
-import styles from './page.module.css';
+//import styles from './page.module.css';
 
 export default function Page() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.heading}>Registrar Egreso</h1>
-      <EgresoForm />
-    </main>
+   <main>
+  <h1>Registrar Egreso</h1>
+  <EgresoForm />
+</main>
+
   );
   //decia que solucionaba//
 }
